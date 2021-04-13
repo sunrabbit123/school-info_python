5abd09482297c97ab772b089c68addaa9fda13cf
sunrabbit123 <qudwls185@naver.com>
2021-04-13 10:32:18 +0900

    ADD: bring meal

--- schoolInfo/school/meal/meal.py
+++ schoolInfo/school/meal/meal.py
@@ -5,12 +5,21 @@ import re
 
 import schoolInfo.util as util
 
-async def meal(ATPT_OFCDC_SC_CODE : str, SD_SCHUL_CODE : str, MLSV_YMD : datetime.datetime, timezone : str = "Asia/Seoul") -> dict :
-    MLSV_YMD = datetime.datetime.now(tz(timezone))
+
+async def meal(
+    ATPT_OFCDC_SC_CODE: str,
+    SD_SCHUL_CODE: str,
+    MLSV_YMD: datetime.datetime = None,
+    timezone: str = "Asia/Seoul",
+) -> dict:
+    MLSV_YMD = datetime.datetime.now(tz(timezone)) if MLSV_YMD is None else MLSV_YMD
     MLSV__YMD = re.sub("[^0-9]", "", str(MLSV_YMD.__str__()))[2:8]
 
-
-
-    option = [f"MLSV_YMD={MLSV__YMD}", f"ATPT_OFCDC_SC_CODE={ATPT_OFCDC_SC_CODE}", f"SD_SCHUL_CODE={SD_SCHUL_CODE}"]
-    url : str = util.url_manager(type="mealServiceDietInfo").url
-    return await util.HTMLGetter().get_json(url)
+    option = [
+        f"MLSV_YMD={MLSV__YMD}",
+        f"ATPT_OFCDC_SC_CODE={ATPT_OFCDC_SC_CODE}",
+        f"SD_SCHUL_CODE={SD_SCHUL_CODE}",
+    ]
+    url: str = util.url_manager(type="mealServiceDietInfo", additions=option).url
+    meal_data = (await util.HTMLGetter().get_json(url))["mealServiceDietInfo"][1]["row"]
+    return meal_data
