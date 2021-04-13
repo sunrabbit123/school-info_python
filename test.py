e5a7bf1daf5d4e120baf8556835d57db7122e49e
sunrabbit123 <qudwls185@naver.com>
2021-04-13 10:35:29 +0900

    update: add print end line

--- test.py
+++ test.py
@@ -5,13 +5,13 @@ import schoolInfo
 
 async def main():
     school_data: dict = await schoolInfo.search("광주소프트웨어마이스터고등학교")
-    print(school_data)
+    print(school_data, end="\n\n\n\n\n")
 
     meal_data: dict = await schoolInfo.meal(
         ATPT_OFCDC_SC_CODE=school_data["ATPT_OFCDC_SC_CODE"],
         SD_SCHUL_CODE=school_data["SD_SCHUL_CODE"],
     )
-    print(meal_data)
+    print(meal_data, end="\n\n\n\n\n")
 
 
 if __name__ == "__main__":
