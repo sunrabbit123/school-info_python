790b9d70692afdcb4662eed4d5bfdf9d5e47e2d5
sunrabbit123 <qudwls185@naver.com>
2021-04-13 10:32:33 +0900

    Update: bring information form

--- schoolInfo/school/search/search.py
+++ schoolInfo/school/search/search.py
@@ -2,6 +2,9 @@ import asyncio
 
 from schoolInfo.util import url_manager, HTMLGetter
 
-async def search(keyword : str) -> dict:
-    url : str = url_manager(type="schoolInfo")
-    return await HTMLGetter().get_json(url)
+
+async def search(keyword: str) -> dict:
+    url: str = url_manager(
+        type="schoolInfo", additions=[f"SCHUL_NM={keyword}"]
+    ).get_url()
+    return (await HTMLGetter().get_json(url))["schoolInfo"][1]["row"][0]
