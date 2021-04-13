848c6778b314dfcf2e596eb7cd0d27a73eb96a42
sunrabbit123 <qudwls185@naver.com>
2021-04-13 10:32:05 +0900

    MISC: using formatting module(black)

--- test.py
+++ test.py
@@ -2,13 +2,16 @@ import asyncio
 
 import schoolInfo
 
-async def main():
-    school_data : dict = await schoolInfo.search("광주소프트웨어마이스터고등학교")
-    print(school_data)
-    
-    meal_data : dict = await schoolInfo.meal(ATPT_OFCDC_SC_CODE=school_data["ATPT_OFCDC_SC_CODE"], SD_SCHUL_CODE=school_data["SD_SCHUL_CODE"])
-    print(meal_data)
 
+async def main():
+    school_data: dict = await schoolInfo.search("광주소프트웨어마이스터고등학교")
+    print(school_data)
+
+    meal_data: dict = await schoolInfo.meal(
+        ATPT_OFCDC_SC_CODE=school_data["ATPT_OFCDC_SC_CODE"],
+        SD_SCHUL_CODE=school_data["SD_SCHUL_CODE"],
+    )
+    print(meal_data)
 
 
 if __name__ == "__main__":
