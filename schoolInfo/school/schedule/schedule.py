9a38c4017c674f4948f9fba9c7a98f40105e9384
sunrabbit123 <qudwls185@naver.com>
2021-04-13 22:29:16 +0900

    misc: using formatter black

--- schoolInfo/school/schedule/schedule.py
+++ schoolInfo/school/schedule/schedule.py
@@ -5,7 +5,12 @@ import re
 from schoolInfo.util import url_manager, HTMLGetter
 
 
-async def schedule(ATPT_OFCDC_SC_CODE: str, SD_SCHUL_CODE: str, start_date: datetime.datetime = None, end_date : datetime.datetime= None):
+async def schedule(
+    ATPT_OFCDC_SC_CODE: str,
+    SD_SCHUL_CODE: str,
+    start_date: datetime.datetime = None,
+    end_date: datetime.datetime = None,
+):
     try:
         start_date = re.sub("[^0-9]", "", str(start_date.__str__()))[2:8]
         end_date = re.sub("[^0-9]", "", str(end_date.__str__()))[2:8]
@@ -15,10 +20,8 @@ async def schedule(ATPT_OFCDC_SC_CODE: str, SD_SCHUL_CODE: str, start_date: date
     addition = [
         f"ATPT_OFCDC_SC_CODE={ATPT_OFCDC_SC_CODE}",
         f"SD_SCHUL_CODE={SD_SCHUL_CODE}",
-        f"AA_FROM_YMD={start_date}"
-        f"AA_TO_YMD={end_date}"
+        f"AA_FROM_YMD={start_date}" f"AA_TO_YMD={end_date}",
     ]
 
     url: str = url_manager(type="SchoolSchedule", additions=addition).get_url()
-    print(url)
     return (await HTMLGetter().get_json(url))["SchoolSchedule"][1]["row"]
