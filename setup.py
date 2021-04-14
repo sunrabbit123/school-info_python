2e3c67e6f8d26433cb9c0d4a41b1f8d53b651d04
sunrabbit123 <qudwls185@naver.com>
2021-04-14 12:11:05 +0900

    MISC: using formater black

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 from setuptools import setup, find_packages
 
-setup(name='schoolInfo', 
-      version='1.0.0', 
-      url='https://github.com/sunrabbit123/school-info_python',
-      author='sunrabbit123', 
-      author_email='qudwls185@naver.com',
-      description='간단하게 Neis API를 이용해보세요!', 
-      packages=find_packages(exclude=['tests']), 
-      long_description=open('README.md', encoding='UTF-8').read(),
-      long_description_content_type='text/markdown',
-      install_requires=["aiohttp", "asyncio", "pytz"],
-      python_requires=">=3.6",
-      zip_safe=False,
-      classifiers=[
-          'License :: OSI Approved :: MIT License'
-      ] 
+setup(
+    name="schoolInfo",
+    version="1.0.0",
+    url="https://github.com/sunrabbit123/school-info_python",
+    author="sunrabbit123",
+    author_email="qudwls185@naver.com",
+    description="간단하게 Neis API를 이용해보세요!",
+    packages=find_packages(exclude=["tests"]),
+    long_description=open("README.md", encoding="UTF-8").read(),
+    long_description_content_type="text/markdown",
+    install_requires=["aiohttp", "asyncio", "pytz"],
+    python_requires=">=3.6",
+    zip_safe=False,
+    classifiers=["License :: OSI Approved :: MIT License"],
 )
--- tests/test.py
+++ tests/test.py
@@ -4,7 +4,7 @@ import schoolInfo
 
 
 async def main():
-    print_end : str = "\n\n\n\n\n"
+    print_end: str = "\n\n\n\n\n"
     school_data: dict = await schoolInfo.search("광주소프트웨어마이스터고등학교")
     print(school_data, end=print_end)
 
@@ -35,14 +35,15 @@ async def main():
 
     print(class_data, end=print_end)
 
-    timetable_data : dict = await schoolInfo.timetable(
+    timetable_data: dict = await schoolInfo.timetable(
         ATPT_OFCDC_SC_CODE=school_data["ATPT_OFCDC_SC_CODE"],
         SD_SCHUL_CODE=school_data["SD_SCHUL_CODE"],
-        SCHUL_KND_SC_NM=school_data["SCHUL_KND_SC_NM"]
+        SCHUL_KND_SC_NM=school_data["SCHUL_KND_SC_NM"],
     )
 
     print(timetable_data, end=print_end)
-    
+
+
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
