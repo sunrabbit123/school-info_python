cb3cd938f1ba40f1f567aaa7a8853c8c30d4cf45
sunrabbit123 <qudwls185@naver.com>
2021-04-17 17:18:48 +0900

    update: decorator error except

--- schoolInfo/util/decorater.py
+++ schoolInfo/util/decorater.py
@@ -1,7 +1,10 @@
+from functools import wraps
+
 def except_keyError(func):
-    def wrapper():
+    @wraps(func)
+    def wrapper(*args, **kwargs):
         try:
-            func()
+            return func(*args, **kwargs)
         except KeyError:
             raise KeyError("잘못된 인자값 또는 값이 없습니다.")
 
