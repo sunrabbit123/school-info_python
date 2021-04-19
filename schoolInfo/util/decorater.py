250b77b9578618738b4bca6618cf188a607e1307
sunrabbit123 <qudwls185@naver.com>
2021-04-19 20:43:45 +0900

    add: 데코레이터의 비동기 처리 추가

--- schoolInfo/util/decorater.py
+++ schoolInfo/util/decorater.py
@@ -2,9 +2,9 @@ from functools import wraps
 
 def except_keyError(func):
     @wraps(func)
-    def wrapper(*args, **kwargs):
+    async def wrapper(*args, **kwargs):
         try:
-            return func(*args, **kwargs)
+            return await func(*args, **kwargs)
         except KeyError:
             raise KeyError("잘못된 인자값 또는 값이 없습니다.")
 
